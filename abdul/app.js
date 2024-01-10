616df24131e70d58f7d707f0980dc7bffc492d48
Abdul4real <137358627+Abdul4real@users.noreply.github.com>
2024-01-10 01:54:06 -0500

    test1

---
+++ abdul/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 3000;
+const path = require('path');
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const indexRoute = require('./routes/index.js');
+
+app.get('/', indexRoute);
+
+app.post("/contact", indexRoute)
+
+
+
+
+app.listen(process.env.PORT || port, function() {
+    console.log('Server has started sucessfully on port',port);
+  });
